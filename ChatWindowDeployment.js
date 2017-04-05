112e4e500028ba98348f51a9a012b8583cbf3fc5
Vuk Dukic <vuk.dukic@clara.com>
2017-04-05 13:37:51 -0700

    Create ChatWindowDeployment.js

---
+++ ChatWindowDeployment.js
@@ -0,0 +1,4 @@
+<script type='text/javascript' src='https://c.la2-c1-dfw.salesforceliveagent.com/content/g/js/39.0/deployment.js'></script>
+<script type='text/javascript'>
+liveagent.init('https://d.la2-c1-dfw.salesforceliveagent.com/chat', '572410000004bST', '00D41000002m9QU');
+</script>
