fa17fe85155fa35c22264063b66fa02f1eb602b5
Vuk Dukic <vuk.dukic@clara.com>
2017-04-05 13:34:29 -0700

    Create ChatButton.js

---
+++ ChatButton.js
@@ -0,0 +1,7 @@
+<img id="liveagent_button_online_573410000004eKF" style="display: none; border: 0px none; cursor: pointer" onclick="liveagent.startChat('573410000004eKF')" src="https://joeytravel-developer-edition.na35.force.com/livechat/resource/1491366209000/OnlineButton" /><img id="liveagent_button_offline_573410000004eKF" style="display: none; border: 0px none; " src="https://joeytravel-developer-edition.na35.force.com/livechat/resource/1491366248000/OfflineButton" />
+<script type="text/javascript">
+if (!window._laq) { window._laq = []; }
+window._laq.push(function(){liveagent.showWhenOnline('573410000004eKF', document.getElementById('liveagent_button_online_573410000004eKF'));
+liveagent.showWhenOffline('573410000004eKF', document.getElementById('liveagent_button_offline_573410000004eKF'));
+});</script>
+
